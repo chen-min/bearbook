67be87040fefde25a1baf8ff28224dc790a6946a
chenm <chenmin@hotmail.de>
2018-12-14 21:00:15 +0800

    数据劫持与代理

--- mvvm.js
+++ mvvm.js
@@ -1,4 +1,32 @@
-function Wue(options = {}){
-    console.log('mvvm')
+
+function Wue(options={}){
     this.$options = options
+    var data = this._data = this.$options.data
+    observe(data);
 }
+
+function Observe(data) { 
+    for(let key in data){ 
+        let value = data[key]
+        observe(value);
+        Object.defineProperty(data, key , {
+            enumerable: true,
+            get(){
+                return value;
+            },
+            set(newValue){ 
+                if(newValue === value){
+                    return ;
+                }
+                value = newValue; 
+                observe(newValue)
+            }
+        })
+    }
+}
+
+
+function observe(data) {
+    return new Observe(data)
+}
+
