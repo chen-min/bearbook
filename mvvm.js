3170df62233b8e58daddb984c68a6dfcc56bb2c8
chenm <chenmin@hotmail.de>
2018-12-17 22:05:11 +0800

    模板编译

--- mvvm.js
+++ mvvm.js
@@ -3,10 +3,23 @@ function Wue(options={}){
     this.$options = options
     var data = this._data = this.$options.data
     observe(data);
+
+    for(let  key in data){
+        Object.defineProperty(this, key,{
+            enumerable: true,
+            get(){
+                return this._data[key];
+            },
+            set(newVal){
+                this._data[key] = newVal
+            }
+        })
+    }
+
 }
 
-function Observe(data) { 
-    for(let key in data){ 
+function Observe(data) { //这里写主要逻辑
+    for(let key in data){ //把data属性通过objectdefineproperty方式定义属性
         let value = data[key]
         observe(value);
         Object.defineProperty(data, key , {
@@ -16,7 +29,7 @@ function Observe(data) {
             },
             set(newValue){ 
                 if(newValue === value){
-                    return ;
+                     return ;
                 }
                 value = newValue; 
                 observe(newValue)
